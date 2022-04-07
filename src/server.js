b287a8ae61df4ccbdd170a56349d3bcee8be3308
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-04-07 13:49:56 -0500

    routes for socket updated

--- src/server.js
+++ src/server.js
@@ -7,15 +7,21 @@ const { socketRouter } = require("./socket/config");
 const app = express();
 const port = 3000;
 
-app.use(morgan("default"));
+app.use(morgan("combined"));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World Node!');
 })
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 })
 
-const io = socket(server);
+const io = socket(server,{
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: false
+      }
+});
 socketRouter(io);
--- src/socket/config.js
+++ src/socket/config.js
@@ -1,18 +1,53 @@
 const routes = require("./routes");
+const RoomsManager = require("./rooms");
+var rooms = new RoomsManager();
 
-const socketRouter = (io) => {
+const socketRouter = (server) => {
 
-    io.on("connect", (socket) => {
-        console.log("welcome: ")
+    server.on("connection", (client) => {
+        console.log("welcome: ", client.id);
+
+        client.on("disconnect", () => {
+            console.log("Bye: ", client.id);
+
+            if(client.isWeb){
+                rooms.removeWebClient(client.room, client.id);
+            }else{
+                rooms.removeClient(client.room, client.id);
+            }
+
+            if(client.room){
+                client.leave(client.room);
+            }
+
+            console.log("Rooms: ", rooms.get())
+        });
+
+        client.on(routes.JOIN_ROOM_WEB, (room) => {
+            client.room = room;
+            client.isWeb = true;
+            client.join(room);
+            rooms.addWebClient(room, client.id);
+            console.log("Web client | Rooms: ", rooms.get())
+        });
+
+        client.on(routes.JOIN_ROOM_CLIENT, (room) => {
+            client.room = room;
+            client.isWeb = false;
+            client.join(room);
+            rooms.addClient(room, client.id);
+            console.log("Client | Rooms: ", rooms.get())
+        });
+
+
+        client.on(routes.STREAM_CLIENT_SERVER, (frame) => {
+            if(client.room){
+                server.to(client.room).emit(routes.STREAM_SERVER_WEB, frame);   
+            }
+        })
     });
 
-    io.on("disconnect", (socket) => {
-        console.log("Bye: ");
-    });
 
-    io.on(routes.STREAM_CLIENT_SERVER, (socket) => {
-        socket.broadcast.emit(routes.STREAM_SERVER_WEB);
-    })
 
     console.log("configuring routes of socket...");
 }
